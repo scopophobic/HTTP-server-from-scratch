4cab7d536beaebea5c762e86b2092ab871aeef95
scopophobic <adithyanmadhu1234@hmail.com>
2025-02-12 14:21:14 +0530

    server not found message

--- app/server.go
+++ app/server.go
@@ -6,23 +6,34 @@ import (
 	"os"
 )
 
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-
-	fmt.Println("Logs from your program will appear here!")
+	fmt.Println("Server is listening on port 4221...")
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		fmt.Println("Failed to bind to port 4221:", err)
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+
+		// Handle the connection in a separate goroutine
+		go handleConnection(conn)
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("New client connected")
+
+	_, err := conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Error writing to connection:", err)
+	}
 }
